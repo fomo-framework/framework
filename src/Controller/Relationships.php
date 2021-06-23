dd07c6af6d9b0448a996c544148c508ad13ff2e0
Amir <faramarzii.amir@gmail.com>
2021-06-23 11:28:33 +0430

    add trait relationships

---
+++ src/Controller/Relationships.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace Tower\Controller;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Tower\DB;
+
+trait Relationships
+{
+    private array $columns = ['*'];
+    private array $orderBy = [];
+    private array $withPivot = [];
+    private string $identity = 'id';
+    private ?int $limit = null;
+
+    protected function select(array $columns): self
+    {
+        if (in_array('*' , $this->columns))
+            unset($this->columns[0]);
+
+        $this->columns = array_merge($this->columns , $columns);
+
+        return $this;
+    }
+
+    protected function identity(string $identity): self
+    {
+        $this->identity = $identity;
+
+        return $this;
+    }
+
+    protected function limit(int $limit): self
+    {
+        $this->limit = $limit;
+
+        return $this;
+    }
+
+    protected function withPivot(array $columns): self
+    {
+        $this->withPivot = array_merge($this->withPivot , $columns);
+
+        return $this;
+    }
+
+    protected function withTimestamp(): self
+    {
+        $this->withPivot = array_merge($this->withPivot , ['created_at' , 'updated_at']);
+
+        return $this;
+    }
+
+    protected function orderBy(string $column , string $direction = 'desc'): self
+    {
+        $this->orderBy[0] = $column;
+        $this->orderBy[1] = $direction;
+
+        return $this;
+    }
+
+    protected function hasOne(Collection|Paginator $data , string $table , string $foreignKey, string $localKey = null): void
+    {
+        if (! in_array('*' , $this->columns) && ! in_array($foreignKey , $this->columns))
+            $this->select([$foreignKey]);
+
+        $relationships = $this->setHasRelationships($data , $table , $foreignKey , $localKey);
+
+        $localKey = $relationships[1];
+        $relationships = $relationships[0];
+
+        $table = Str::singular($table);
+        $data->map(function ($item) use ($foreignKey , $localKey , $table , $relationships) {
+            $item->$table = $relationships->where($foreignKey , $item->$localKey)->first();
+        });
+    }
+
+    protected function  hasMany(Collection|Paginator $data , string $table , string $foreignKey, string $localKey = null): void
+    {
+        if (! in_array('*' , $this->columns) && ! in_array($foreignKey , $this->columns))
+            $this->select([$foreignKey]);
+
+        $relationships = $this->setHasRelationships($data , $table , $foreignKey , $localKey);
+
+        $localKey = $relationships[1];
+        $relationships = $relationships[0];
+
+        $table = Str::plural($table);
+
+        if (is_null($this->limit))
+            $data->map(function ($item) use ($foreignKey , $localKey , $table , $relationships) {
+                $item->$table = $relationships->where($foreignKey , $item->$localKey)->values();
+            });
+        else
+            $data->map(function ($item) use ($foreignKey , $localKey , $table , $relationships) {
+                $item->$table = $relationships->where($foreignKey , $item->$localKey)->take($this->limit)->values();
+            });
+    }
+
+    protected function throughOne(Collection|Paginator $data , string $interfaceTable , string $table, string $foreignKey, string $localKey): void
+    {
+        $relationships = $this->setThrough($data , $interfaceTable , $table , $foreignKey , $localKey);
+
+        $table = Str::singular($table);
+        $data->map(function ($item) use ($foreignKey , $localKey , $table , $relationships) {
+            $item->$table = $relationships->where("pivot_$localKey" , $item->id)->first();
+        });
+    }
+
+    protected function throughManyToMany(Collection|Paginator $data , string $interfaceTable , string $table, string $foreignKey, string $localKey): void
+    {
+        $relationships = $this->setThrough($data , $interfaceTable , $table , $foreignKey , $localKey);
+
+        $table = Str::plural($table);
+
+        if (is_null($this->limit))
+            $data->map(function ($item) use ($foreignKey , $localKey , $table , $relationships) {
+                $item->$table = $relationships->where("pivot_$localKey" , $item->id)->values();
+            });
+        else
+            $data->map(function ($item) use ($foreignKey , $localKey , $table , $relationships) {
+                $item->$table = $relationships->where("pivot_$localKey" , $item->id)->take($this->limit)->values();
+            });
+    }
+
+    protected function polymorphicHasOne(Collection|Paginator $data , string $table , string $type , string $typeName = null , string $idName = null , string $localKey = null): void
+    {
+        $polymorphic = $this->setPolymorphic($data , $table , $type , $typeName , $idName , $localKey);
+
+        $idName = $polymorphic[1];
+        $localKey = $polymorphic[2];
+        $relationships = $polymorphic[0];
+
+        $table = Str::singular($table);
+        $data->map(function ($item) use ($idName , $localKey , $relationships , $table) {
+            $item->$table = $relationships->where($idName , $item->$localKey)->first();
+        });
+    }
+
+    protected function polymorphicManyToMany(Collection|Paginator $data , string $table , string $type , string $typeName = null , string $idName = null , string $localKey = null): void
+    {
+        $polymorphic = $this->setPolymorphic($data , $table , $type , $typeName , $idName , $localKey);
+
+        $idName = $polymorphic[1];
+        $localKey = $polymorphic[2];
+        $relationships = $polymorphic[0];
+
+        $table = Str::plural($table);
+
+        if (is_null($this->limit))
+            $data->map(function ($item) use ($idName , $localKey , $relationships , $table) {
+                $item->$table = $relationships->where($idName , $item->$localKey)->values();
+            });
+        else
+            $data->map(function ($item) use ($idName , $localKey , $relationships , $table) {
+                $item->$table = $relationships->where($idName , $item->$localKey)->take($this->limit)->values();
+            });
+    }
+
+    private function setPolymorphic(Collection|Paginator $data , string $table , string $type , string $typeName = null , string $idName = null , string $localKey = null): array
+    {
+        $filedName = Str::singular($table) . 'able_';
+        $typeName = $typeName ? $filedName . $typeName : $filedName . 'type';
+        $idName = $idName ? $filedName . $idName : $filedName . 'id';
+        $localKey = $localKey ?: 'id';
+
+        if (! in_array('*' , $this->columns) && ! in_array($idName , $this->columns))
+            $this->select([$idName]);
+
+        if (empty($this->orderBy))
+            $relationships = DB::table($table)
+                ->whereIn($idName , $data->pluck($localKey)->toArray())
+                ->where($typeName , $type)
+                ->select($this->columns)->get();
+        else
+            $relationships = DB::table($table)
+                ->whereIn($idName , $data->pluck($localKey)->toArray())
+                ->where($typeName , $type)
+                ->select($this->columns)->orderBy($this->orderBy[0] , $this->orderBy[1])->get();
+
+        return [$relationships , $idName , $localKey];
+    }
+
+    private function setThrough(Collection|Paginator $data , string $interfaceTable , string $table, string $foreignKey, string $localKey): Collection
+    {
+        $this->withPivot([$localKey]);
+
+        array_walk($this->withPivot , function (&$value) use ($interfaceTable) {
+            $value = "$interfaceTable.$value as pivot_$value";
+        });
+
+        array_walk($this->columns , function (&$value) use ($table) {
+            $value = "$table.$value";
+        });
+
+        $columns = array_merge($this->columns , $this->withPivot);
+
+        if (empty($this->orderBy))
+            $relationships = DB::table($table)
+                ->join($interfaceTable , "$table.$this->identity", '=' , "$interfaceTable.$foreignKey")
+                ->whereIn("$interfaceTable.$localKey" , $data->pluck($this->identity)->toArray())
+                ->select($columns)
+                ->get();
+        else
+            $relationships = DB::table($table)
+                ->join($interfaceTable , "$table.$this->identity", '=' , "$interfaceTable.$foreignKey")
+                ->whereIn("$interfaceTable.$localKey" , $data->pluck($this->identity)->toArray())
+                ->orderBy($this->orderBy[0] , $this->orderBy[1])
+                ->select($columns)
+                ->get();
+
+        return $relationships;
+    }
+
+    private function setHasRelationships(Collection|Paginator $data , string $table , string $foreignKey, string $localKey = null): array
+    {
+        $localKey = $localKey ?: 'id';
+
+        if (empty($this->orderBy))
+            $relationships = DB::table($table)
+                ->whereIn($foreignKey , $data->pluck($localKey)->toArray())
+                ->select($this->columns)
+                ->get();
+        else
+            $relationships = DB::table($table)
+                ->whereIn($foreignKey , $data->pluck($localKey)->toArray())
+                ->select($this->columns)
+                ->orderBy($this->orderBy[0] , $this->orderBy[1])
+                ->get();
+
+        return [$relationships , $localKey];
+    }
+}
