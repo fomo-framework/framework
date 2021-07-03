886714142153c4e4825de77b72f748885b6a1997
Amir <faramarzii.amir@gmail.com>
2021-07-03 16:49:19 +0430

    bug fix

--- src/JobWorker.php
+++ src/JobWorker.php
@@ -18,41 +18,32 @@ class JobWorker extends Kernel
 
     protected function listen(): void
     {
-        Timer::add(0.00001 ,  function (){
+        Timer::add(1 ,  function (){
             $queue = Redis::getInstance()->lPop(env('APP_NAME' , 'tower') . 'Queue');
             if ($queue){
                 $data = json_decode($queue);
-                try {
-                    $class = new $this->jobs[$data->queue]();
-                    if (method_exists($class ,'handle'))
-                        call_user_func_array([$class , 'handle'] , [$data->data]);
-                }catch (Throwable $e)
-                {
-                    $class = new $this->jobs[$data->queue]();
-                    if ($data->attempts > 1){
-                        if (method_exists($class ,'retry')){
-                            call_user_func_array([$class , 'retry'] , [$data->data , $data->attempts]);
-                        }else{
-                            try {
-                                throw new QueueException('retry' , $data->queue , (array) $data->data , $e->getMessage() , $e->getFile() , $e->getLine());
-                            }catch (QueueException $e)
-                            {
-                                $e->handle();
-                            }
+                if (isset($this->jobs[$data->queue])){
+                    try {
+                        $class = new $this->jobs[$data->queue]();
+                        if (method_exists($class ,'handle'))
+                            call_user_func_array([$class , 'handle'] , [$data->data]);
+                    }catch (Throwable $e)
+                    {
+                        $class = new $this->jobs[$data->queue]();
+                        if ($data->attempts > 1){
+                            (new QueueException('retry' , $data->queue , (array) $data->data , $e->getMessage() , $e->getFile() , $e->getLine()))->handle();
                             (new Queue())->store($data->queue , (array) $data->data , $data->attempts - 1);
-                        }
-                    }else {
-                        if (method_exists($class ,'failed')){
-                            call_user_func_array([$class , 'failed'] , [$data->data]);
-                        }else{
-                            try {
-                                throw new QueueException('failed' , $data->queue , (array) $data->data , $e->getMessage() , $e->getFile() , $e->getLine());
-                            }catch (QueueException $e)
-                            {
-                                $e->handle();
+                        }else {
+                            if (method_exists($class ,'failed')){
+                                call_user_func_array([$class , 'failed'] , [$data->data]);
+                            }else{
+                                (new QueueException('failed' , $data->queue , (array) $data->data , $e->getMessage() , $e->getFile() , $e->getLine()))->handle();
                             }
                         }
                     }
+                }else{
+                    $values = json_encode($data->data);
+                    (new Log())->channel('queue')->alert("not found handler [queue: $data->queue] [data: $values]");
                 }
             }
         });
