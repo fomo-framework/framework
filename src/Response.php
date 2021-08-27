3edb141532b6b4cc23e04a132c3d33edd6a0b92d
Amir <faramarzii.amir@gmail.com>
2021-08-27 13:03:00 +0430

    add new method to src/Response.php and set ext method to composer.json

--- src/Response.php
+++ src/Response.php
@@ -2,6 +2,7 @@
 
 namespace Tower;
 
+use SimpleXMLElement;
 use Workerman\Protocols\Http\Response as WorkerResponse;
 
 class Response extends WorkerResponse
@@ -69,4 +70,105 @@ class Response extends WorkerResponse
     public const HTTP_LOOP_DETECTED = 508;                                               // RFC5842
     public const HTTP_NOT_EXTENDED = 510;                                                // RFC2774
     public const HTTP_NETWORK_AUTHENTICATION_REQUIRED = 511;
+
+    public const PHRASES = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-status',
+        208 => 'Already Reported',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Large',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested range not satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        511 => 'Network Authentication Required',
+    ];
+
+    public function json(array $data, int $status = 200): self
+    {
+        $this->_status = $status;
+        $this->_body = json_encode($data , JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_IGNORE);
+        $this->_header = ['Content-Type' => 'application/json'];
+
+        return $this;
+    }
+
+    public function noContent(): self
+    {
+        $this->_status = 204;
+
+        return $this;
+    }
+
+    public function xml(mixed $xml , int $status = 200): self
+    {
+        if ($xml instanceof SimpleXMLElement)
+            $xml = $xml->asXML();
+
+        $this->_status = $status;
+        $this->_body = $xml;
+        $this->_header = ['Content-Type' => 'text/xml'];
+
+        return $this;
+    }
+
+    public function jsonp(mixed $data, string $callbackName = 'callback' , int $status = 200): self
+    {
+        if (!is_scalar($data) && null !== $data)
+            $data = json_encode($data);
+
+        $this->_body = "$callbackName($data)";
+        $this->_status = $status;
+        $this->_header = ['Content-Type' => 'application/json'];
+
+        return $this;
+    }
 }
