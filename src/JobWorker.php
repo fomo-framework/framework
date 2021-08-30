82601ab5dafd1f131f0ed7af827426460541505d
Amir <faramarzii.amir@gmail.com>
2021-08-30 15:41:18 +0430

    bug fix in JobWorker.php

--- src/JobWorker.php
+++ src/JobWorker.php
@@ -27,37 +27,42 @@ class JobWorker extends Kernel
     protected function listen(): void
     {
         Timer::add(1 ,  function (){
-            $queue = Redis::getInstance()->lPop(env('APP_NAME' , 'tower') . 'Queue');
-            if ($queue){
-                $data = json_decode($queue);
-                if (isset($this->jobs[$data->queue])){
-                    try {
-                        $class = new $this->jobs[$data->queue]();
-                        if (method_exists($class ,'handle'))
-                            call_user_func_array([$class , 'handle'] , [$data->data]);
-                    }catch (Exception $e)
-                    {
-                        $class = new $this->jobs[$data->queue]();
-                        if ($data->attempts > 1){
-                            (new QueueException('retry' , $data->queue , (array) $data->data , $e->getMessage() , $e->getFile() , $e->getLine()))->handle();
-                            (new Queue())->store($data->queue , (array) $data->data , $data->attempts - 1);
-                        }else {
-                            if (method_exists($class ,'failed')){
-                                try {
-                                    call_user_func_array([$class , 'failed'] , [$data->data]);
-                                }catch (Exception $exception){
-                                    (new QueueException('failed' , $data->queue , (array) $data->data , $exception->getMessage() , $exception->getFile() , $exception->getLine()))->handle();
+            try {
+                $queue = Redis::getInstance()->lPop(env('APP_NAME' , 'tower') . 'Queue');
+                if ($queue){
+                    $data = json_decode($queue);
+                    if (isset($this->jobs[$data->queue])){
+                        try {
+                            $class = new $this->jobs[$data->queue]();
+                            if (method_exists($class ,'handle'))
+                                call_user_func_array([$class , 'handle'] , [$data->data]);
+                        }catch (Exception $e)
+                        {
+                            $class = new $this->jobs[$data->queue]();
+                            if ($data->attempts > 1){
+                                (new QueueException('retry' , $data->queue , (array) $data->data , $e->getMessage() , $e->getFile() , $e->getLine()))->handle();
+                                (new Queue())->store($data->queue , (array) $data->data , $data->attempts - 1);
+                            }else {
+                                if (method_exists($class ,'failed')){
+                                    try {
+                                        call_user_func_array([$class , 'failed'] , [$data->data]);
+                                    }catch (Exception $exception){
+                                        (new QueueException('failed' , $data->queue , (array) $data->data , $exception->getMessage() , $exception->getFile() , $exception->getLine()))->handle();
+                                    }
+                                }else{
+                                    $values = json_encode($data->data);
+                                    (new Log())->channel('queue')->alert("not found failed handler [queue: $data->queue] [data: $values]");
                                 }
-                            }else{
-                                $values = json_encode($data->data);
-                                (new Log())->channel('queue')->alert("not found failed handler [queue: $data->queue] [data: $values]");
                             }
                         }
+                    }else{
+                        $values = json_encode($data->data);
+                        (new Log())->channel('queue')->alert("not found handler [queue: $data->queue] [data: $values]");
                     }
-                }else{
-                    $values = json_encode($data->data);
-                    (new Log())->channel('queue')->alert("not found handler [queue: $data->queue] [data: $values]");
                 }
+            }catch (RedisException $e)
+            {
+                (new Log())->channel('queue')->alert($e->getMessage());
             }
         });
     }
@@ -79,7 +84,7 @@ class JobWorker extends Kernel
             Redis::setInstance();
         } catch (RedisException $e)
         {
-            (new Log())->alert($e->getMessage());
+            (new Log())->channel('queue')->alert($e->getMessage());
         }
     }
 }
