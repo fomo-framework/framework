f19ee327c63dc80bd02bfa7c55f4e11bd15d8653
Amir <faramarzii.amir@gmail.com>
2021-09-04 22:26:58 +0430

    bug fix in src/JobWorker.php

--- src/JobWorker.php
+++ src/JobWorker.php
@@ -69,11 +69,9 @@ class JobWorker extends Kernel
 
     protected function setDatabase(): void
     {
-        $config = include configPath() . "database.php";
-
         $capsule = new Capsule();
 
-        $capsule->addConnection($config['mysql']);
+        $capsule->addConnection(Loader::get('database')['mysql']);
 
         $capsule->setAsGlobal();
     }
