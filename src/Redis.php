f59f8fdcc9c7dba3536c78ceff81b20f3f1a2bd9
Amir <faramarzii.amir@gmail.com>
2021-09-04 22:28:48 +0430

    bug fix in src/Redis.php

--- src/Redis.php
+++ src/Redis.php
@@ -4,28 +4,28 @@ namespace Tower;
 
 class Redis
 {
-    protected static \Redis $_instance;
+    protected static \Redis $instance;
 
     public static function getInstance(): \Redis
     {
-        return self::$_instance;
+        return self::$instance;
     }
 
     public static function setInstance(): void
     {
-        $config = include configPath() . "redis.php";
+        $config = Loader::get('redis');
 
-        self::$_instance = new \Redis();
+        self::$instance = new \Redis();
 
-        self::$_instance->connect($config['host'] , $config['port']);
-        self::$_instance->select($config['database']);
+        self::$instance->connect($config['host'] , $config['port']);
+        self::$instance->select($config['database']);
 
         if (! is_null($config['username']) && ! is_null($config['password']))
-            self::$_instance->auth([$config['username'] , $config['password']]);
+            self::$instance->auth([$config['username'] , $config['password']]);
     }
 
     public static function __callStatic(string $method, array $arguments)
     {
-        return self::$_instance->$method(...$arguments);
+        return self::$instance->$method(...$arguments);
     }
 }
