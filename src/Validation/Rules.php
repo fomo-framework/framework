191872f3ca7eae216cc913d9e699162fecc78a48
Amir <faramarzii.amir@gmail.com>
2021-09-04 16:26:32 +0430

    refactor src/Validation/Rules.php

--- src/Validation/Rules.php
+++ src/Validation/Rules.php
@@ -9,125 +9,330 @@ trait Rules
 {
     protected function required(array $parameters): void
     {
-        if (! $this->request->input($parameters['ruleName']) || empty($this->request->input($parameters['ruleName'])) || is_null($this->request->input($parameters['ruleName'])))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if (! $item || empty($item)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if (! $data || empty($data)){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function string(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && !is_string($this->request->input($parameters['ruleName'])))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && !is_string($item)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && !is_string($data)){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function integer(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && !is_int($this->request->input($parameters['ruleName'])))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && !is_int($item)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && !is_int($data)){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function boolean(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && !is_bool($this->request->input($parameters['ruleName'])))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && !is_bool($item)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && !is_bool($data)){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function array(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && !is_array($this->request->input($parameters['ruleName'])))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && !is_array($item)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && !is_array($data)){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function email(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && false === filter_var($this->request->input($parameters['ruleName']), FILTER_VALIDATE_EMAIL))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && false === filter_var($item, FILTER_VALIDATE_EMAIL)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && false === filter_var($data , FILTER_VALIDATE_EMAIL)){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function regex(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && !preg_match($parameters['value'], $this->request->input($parameters['ruleName'])))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && !preg_match($parameters['value'], $item)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && !preg_match($parameters['value'], $data)){
             array_push($this->messages , $parameters['message']);
+        }
+    }
+
+    protected function notRegex(array $parameters): void
+    {
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && preg_match($parameters['value'], $item)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && preg_match($parameters['value'], $data)){
+            array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function max(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && is_string($this->request->input($parameters['ruleName'])) && $this->strlen($this->request->input($parameters['ruleName'])) > $parameters['value'])
-            array_push($this->messages , $parameters['message']);
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item){
+                    if (is_string($item) && $this->strlen($item) > $parameters['value']){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                    if (is_int($item) && $item > $parameters['value']){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                }
+            }
+            return;
+        }
 
-        if ($this->request->input($parameters['ruleName']) && is_int($this->request->input($parameters['ruleName'])) && $this->request->input($parameters['ruleName']) > $parameters['value'])
-            array_push($this->messages , $parameters['message']);
+        if ($data){
+            if (is_string($data) && $this->strlen($data) > $parameters['value']){
+                array_push($this->messages , $parameters['message']);
+            }
+            if (is_int($data) && $data > $parameters['value']){
+                array_push($this->messages , $parameters['message']);
+            }
+        }
     }
 
     protected function min(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && is_string($this->request->input($parameters['ruleName'])) && $this->strlen($this->request->input($parameters['ruleName'])) < $parameters['value'])
-            array_push($this->messages , $parameters['message']);
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item){
+                    if (is_string($item) && $this->strlen($item) < $parameters['value']){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                    if (is_int($item) && $item < $parameters['value']){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                }
+            }
+            return;
+        }
 
-        if ($this->request->input($parameters['ruleName']) && is_int($this->request->input($parameters['ruleName'])) && $this->request->input($parameters['ruleName']) < $parameters['value'])
-            array_push($this->messages , $parameters['message']);
+        if ($data){
+            if (is_string($data) && $this->strlen($data) < $parameters['value']){
+                array_push($this->messages , $parameters['message']);
+            }
+            if (is_int($data) && $data < $parameters['value']){
+                array_push($this->messages , $parameters['message']);
+            }
+        }
     }
 
     protected function size(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && is_string($this->request->input($parameters['ruleName'])) && $this->strlen($this->request->input($parameters['ruleName'])) != $parameters['value'])
-            array_push($this->messages , $parameters['message']);
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item){
+                    if (is_string($item) && $this->strlen($item) != $parameters['value']){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                    if (is_int($item) && $item != $parameters['value']){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                }
+            }
+            return;
+        }
 
-        if ($this->request->input($parameters['ruleName']) && is_int($this->request->input($parameters['ruleName'])) && $this->request->input($parameters['ruleName']) != $parameters['value'])
-            array_push($this->messages , $parameters['message']);
+        if ($data){
+            if (is_string($data) && $this->strlen($data) != $parameters['value']){
+                array_push($this->messages , $parameters['message']);
+            }
+            if (is_int($data) && $data != $parameters['value']){
+                array_push($this->messages , $parameters['message']);
+            }
+        }
     }
 
     protected function date(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && ! $this->validateDate($this->request->input($parameters['ruleName']) , is_null($parameters['value']) ? 'Y-m-d H:i:s' : $parameters['value']))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && ! $this->validateDate($item , is_null($parameters['value']) ? 'Y-m-d H:i:s' : $parameters['value'])){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && ! $this->validateDate($data , is_null($parameters['value']) ? 'Y-m-d H:i:s' : $parameters['value'])){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function after(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && $this->request->input($parameters['value']) && $this->request->input($parameters['ruleName']) <= $this->request->input($parameters['value']))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        $value = $this->get($this->data , $parameters['value']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && $value && $item <= $value){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && $value && $data <= $value){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function before(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName']) && $this->request->input($parameters['value']) && $this->request->input($parameters['ruleName']) >= $this->request->input($parameters['value']))
+        $data = $this->get($this->data , $parameters['ruleName']);
+        $value = $this->get($this->data , $parameters['value']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item && $value && $item >= $value){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data && $value && $data >= $value){
             array_push($this->messages , $parameters['message']);
-    }
-
-    protected function validateDate($date, $format = 'Y-m-d H:i:s'): bool
-    {
-        $d = DateTime::createFromFormat($format, $date);
-        return $d && $d->format($format) == $date;
+        }
     }
 
     protected function in(array $parameters): void
     {
+        $data = $this->get($this->data , $parameters['ruleName']);
         $array = explode(',' , $parameters['value']);
-
-        if ($this->request->input($parameters['ruleName'])  && !in_array($this->request->input($parameters['ruleName']) , $array))
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item  && !in_array($item , $array)){
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data  && !in_array($data , $array)){
             array_push($this->messages , $parameters['message']);
+        }
     }
 
     protected function nationalCode(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName'])){
-            if(! preg_match('/^[0-9]{10}$/' , $this->request->input($parameters['ruleName']))){
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item){
+                    if(! preg_match('/^[0-9]{10}$/' , $item)){
+                        array_push($this->messages , $parameters['message']);
+                        return;
+                    }
+
+                    for($i = 0; $i < 10; $i++)
+                        if(preg_match('/^'.$i.'{10}$/' , $item)){
+                            array_push($this->messages , $parameters['message']);
+                            return;
+                        }
+
+                    for($i = 0, $sum = 0; $i < 9; $i++)
+                        $sum += ((10-$i) * intval(substr($item , $i ,1)));
+
+                    $ret = $sum % 11;
+
+                    $parity = intval(substr($item, 9,1));
+
+                    if(($ret < 2 && $ret == $parity) || ($ret >= 2 && $ret == 11 - $parity))
+                        return;
+
+                    array_push($this->messages , $parameters['message']);
+                }
+            }
+            return;
+        }
+        if ($data){
+            if(! preg_match('/^[0-9]{10}$/' , $data)){
                 array_push($this->messages , $parameters['message']);
                 return;
             }
 
             for($i = 0; $i < 10; $i++)
-                if(preg_match('/^'.$i.'{10}$/' , $this->request->input($parameters['ruleName']))){
+                if(preg_match('/^'.$i.'{10}$/' , $data)){
                     array_push($this->messages , $parameters['message']);
                     return;
                 }
 
             for($i = 0, $sum = 0; $i < 9; $i++)
-                $sum += ((10-$i) * intval(substr($this->request->input($parameters['ruleName']) , $i ,1)));
+                $sum += ((10-$i) * intval(substr($data , $i ,1)));
 
             $ret = $sum % 11;
 
-            $parity = intval(substr($this->request->input($parameters['ruleName']), 9,1));
+            $parity = intval(substr($data, 9,1));
 
             if(($ret < 2 && $ret == $parity) || ($ret >= 2 && $ret == 11 - $parity))
                 return;
@@ -136,39 +341,72 @@ trait Rules
         }
     }
 
-    private function checkDB(array $parameters): bool
-    {
-        if (str_contains($parameters['value'] , ','))
-            $table = explode(',' , $parameters['value']);
-
-        if (isset($table))
-            $check = DB::table($table[0])->where($table[1] , $this->request->input($parameters['ruleName']))->exists();
-        else
-            $check = DB::table($parameters['value'])->where('id' , $this->request->input($parameters['ruleName']))->exists();
-
-        return $check;
-    }
-
     protected function exists(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName'])){
-            $check = $this->checkDB($parameters);
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item){
+                    $check = $this->checkDB($item , $parameters['value']);
 
-            if (! $check)
+                    if (!$check){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                }
+            }
+            return;
+        }
+        if ($data){
+            $check = $this->checkDB($data , $parameters['value']);
+
+            if (!$check){
                 array_push($this->messages , $parameters['message']);
+            }
         }
     }
 
     protected function unique(array $parameters): void
     {
-        if ($this->request->input($parameters['ruleName'])){
-            $check = $this->checkDB($parameters);
+        $data = $this->get($this->data , $parameters['ruleName']);
+        if (is_array($data)){
+            foreach ($data as $item){
+                if ($item){
+                    $check = $this->checkDB($item , $parameters['value']);
 
-            if ($check)
+                    if ($check){
+                        array_push($this->messages , $parameters['message']);
+                    }
+                }
+            }
+            return;
+        }
+        if ($data){
+            $check = $this->checkDB($data , $parameters['value']);
+
+            if ($check){
                 array_push($this->messages , $parameters['message']);
+            }
         }
     }
 
+    protected function checkDB(string $item , string $value): bool
+    {
+        if (str_contains($value , ','))
+            $table = explode(',' , $value);
+
+        if (isset($table))
+            $check = DB::table($table[0])->where($table[1] , $item)->exists();
+        else
+            $check = DB::table($value)->where('id' , $item)->exists();
+
+        return $check;
+    }
+
+    protected function validateDate($date, $format = 'Y-m-d H:i:s'): bool
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) == $date;
+    }
 
     protected function strlen($value): bool|int
     {
