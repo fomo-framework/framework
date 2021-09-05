1d35cdd443ac9583b2dd31dd6972650ad3bdc430
Amir <faramarzii.amir@gmail.com>
2021-09-05 16:28:50 +0430

    changes in src/Request.php

--- src/Request.php
+++ src/Request.php
@@ -2,7 +2,7 @@
 
 namespace Tower;
 
-use \Workerman\Protocols\Http\Request as WorkerRequest;
+use Workerman\Protocols\Http\Request as WorkerRequest;
 
 class Request extends WorkerRequest
 {
@@ -37,34 +37,110 @@ class Request extends WorkerRequest
         return mb_substr($header, 7, null, 'UTF-8');
     }
 
-    public function input(string $name, $default = null): mixed
+    public function get($name = null, $default = null): string|int|bool|array|float|null
     {
-        $post = $this->post();
-        if (isset($post[$name])) {
-            return $post[$name];
+        $get = $this->_data['get'];
+
+        if (!isset($get)) {
+            $this->parseGet();
         }
-        $get = $this->get();
-        return $get[$name] ?? $default;
+
+        if (null === $name) {
+            return $get;
+        }
+
+        return $this->getData($get , $name , $default);
     }
 
-    public function all(): mixed
+    public function post($name = null, $default = null): string|int|bool|array|float|null
+    {
+        $post = $this->_data['post'];
+
+        if (!isset($post)) {
+            $this->parsePost();
+        }
+
+        if (null === $name) {
+            return $post;
+        }
+
+        return $this->getData($post , $name , $default);
+    }
+
+    public function header($name = null, $default = null): string|int|bool|array|float|null
+    {
+        $headers = $this->_data['headers'];
+
+        if (!isset($headers)) {
+            $this->parseHeaders();
+        }
+
+        if (null === $name) {
+            return $headers;
+        }
+
+        $name = strtolower($name);
+        return $this->getData($headers , $name , $default);
+    }
+
+    public function cookie($name = null, $default = null): string|int|bool|array|float|null
+    {
+        $cookie = $this->_data['cookie'];
+
+        if (!isset($cookie)) {
+            parse_str(str_replace('; ', '&', $this->header('cookie')), $cookie);
+        }
+
+        if ($name === null) {
+            return $cookie;
+        }
+
+        return $this->getData($cookie , $name , $default);
+    }
+
+    public function file($name = null): string|int|bool|array|float|null
+    {
+        $file = $this->_data['files'];
+
+        if (!isset($file)) {
+            $this->parsePost();
+        }
+
+        if (null === $name) {
+            return $file;
+        }
+
+        return $this->getData($file , $name);
+    }
+
+    public function input(string $name, $default = null): string|int|bool|array|float|null
+    {
+        $post = $this->post($name , $default);
+        if ($post) {
+            return $post;
+        }
+
+        return $this->get($name , $default);
+    }
+
+    public function all(): array
     {
         return $this->post() + $this->get();
     }
 
     public function only(array $keys): array
     {
-        $all = $this->all();
         $result = [];
         foreach ($keys as $key) {
-            if (isset($all[$key])) {
-                $result[$key] = $all[$key];
+            $exists = $this->getData($this->all() , $key);
+            if (!is_null($exists)) {
+                $result[$key] = $exists;
             }
         }
         return $result;
     }
 
-    public function except(array $keys): mixed
+    public function except(array $keys): array
     {
         $all = $this->all();
         foreach ($keys as $key) {
@@ -138,5 +214,26 @@ class Request extends WorkerRequest
         return $this->header('client-ip', $this->header('x-forwarded-for',
             $this->header('x-real-ip', $this->header('x-client-ip',
                 $this->header('via', $this->remoteIp())))));
+    }
+
+    protected function getData(array $array, string $key, ?string $default = null): string|int|bool|array|float|null
+    {
+        if (array_key_exists($key, $array)) {
+            return $array[$key];
+        }
+
+        if (!str_contains($key, '.')) {
+            return $array[$key] ?? value($default);
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (array_key_exists($segment, $array)) {
+                $array = $array[$segment];
+            } else {
+                return value($default);
+            }
+        }
+
+        return $array;
     }
 }
