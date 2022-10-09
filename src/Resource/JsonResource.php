f97f89200bc384967b94c0e735f81528a80f77e0
Amir <faramarzii.amir@gmail.com>
2022-10-09 21:34:40 +0330

    new Resource\JsonResource

---
+++ src/Resource/JsonResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Fomo\Resource;
+
+use Fomo\Response\Response;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use stdClass;
+
+class JsonResource
+{
+    protected Collection|Paginator|stdClass|null $data;
+
+    protected array $response = [];
+
+    protected array $meta = [];
+
+    protected ?int $perPage = null;
+
+    public function __construct(Collection|Paginator|stdClass|array|null $data , int $perPage = null)
+    {
+        if (is_array($data)){
+            $data = $this->arrayToCollection($data);
+        }
+
+        $this->data = $data;
+        $this->perPage = $perPage;
+    }
+
+    public function dontJson(): array|null
+    {
+        if (is_null($this->data)){
+            return null;
+        }
+
+        if ($this->data instanceof stdClass){
+            return $this->toArray($this->data);
+        }
+
+        $this->process();
+
+        return $this->response;
+    }
+
+    protected function arrayToCollection(array $data): Collection
+    {
+        $collection = new Collection();
+
+        foreach ($data as $item){
+            $collection->add((object) $item);
+        }
+
+        return $collection;
+    }
+
+    public function collection(): Response
+    {
+        $this->process();
+
+        if (is_null($this->perPage)){
+            return response()->json([
+                'data' => $this->response ,
+            ]);
+        }
+
+        $this->addToMeta([
+            'isLastPage' => count($this->data) < $this->perPage ,
+            'perPage' => $this->perPage
+        ]);
+
+        return response()->json([
+            'data' => $this->response ,
+            'meta' => $this->meta
+        ]);
+    }
+
+    public function single(): Response
+    {
+        if (is_null($this->data)){
+            return response()->json([
+                'data' => null
+            ]);
+        }
+
+        if ($this->data instanceof Collection){
+            return response()->json([
+                'data' => $this->toArray($this->data->first())
+            ]);
+        }
+
+        return response()->json([
+            'data' => $this->toArray($this->data)
+        ]);
+    }
+
+    protected function process(): void
+    {
+        if (! is_null($this->data)){
+            $this->data->map(function ($data){
+                $this->response[] = $this->toArray($data);
+            });
+        }
+    }
+
+    public function addToMeta(array $values): self
+    {
+        foreach ($values as $key => $value)
+            $this->meta[$key] = $value;
+
+        return $this;
+    }
+
+    protected function toArray($request)
+    {
+        return $request;
+    }
+}
