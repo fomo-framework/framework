fb4b4feca9059cdefc40fb2f8400542e2fdccb7d
Amir <faramarzii.amir@gmail.com>
2022-10-09 22:56:11 +0330

    new Commands\Server\Reload

---
+++ src/Commands/Server/Reload.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Fomo\Commands\Server;
+
+use Swoole\Process;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Fomo\Console\Style;
+
+#[AsCommand(name: 'server:reload' , description: 'reload server')]
+class Reload extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new Style($input, $output);
+
+        if (! httpServerIsRunning()){
+            $io->error('server is not running...' , true);
+            return self::FAILURE;
+        }
+
+        if (httpServerIsRunning()) {
+            Process::kill(getManagerProcessId(), SIGUSR1);
+            Process::kill(getMasterProcessId(), SIGUSR1);
+        }
+
+        $io->success('reloading workers...' , true);
+        return self::SUCCESS;
+    }
+}
