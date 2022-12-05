986e9da68c36de34d8bd99bf8c9583b1270ee07f
Amir <faramarzii.amir@gmail.com>
2022-12-05 21:55:27 +0330

    new Facades\Facade

---
+++ src/Facades/Facade.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Fomo\Facades;
+
+class Facade
+{
+    protected static array $classes = [];
+
+    abstract protected static function getMainClass(): string;
+
+    public static function __callStatic(string $method, array $args)
+    {
+        return self::$classes[static::getMainClass()]->$method(...$args);
+    }
+}
