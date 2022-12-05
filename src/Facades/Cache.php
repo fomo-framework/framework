84e3fb39547720511b006bf843e9a0aaa03c4c41
Amir <faramarzii.amir@gmail.com>
2022-12-05 22:10:42 +0330

    fix(add methods for ide helper) Facades\Cache

--- src/Facades/Cache.php
+++ src/Facades/Cache.php
@@ -2,6 +2,19 @@
 
 namespace Fomo\Facades;
 
+/**
+ * @method static mixed get(string $key , $default = null , int $expire = null)
+ * @method static void put(string $key , $value , int $expire = null)
+ * @method static mixed remember(string $key , int $expire , $value)
+ * @method static mixed rememberForever(string $key , $value)
+ * @method static mixed pull(string $key)
+ * @method static void delete(string $key)
+ * @method static void store(string $key , $value , int $expire = null)
+ * @method static bool has(string $key)
+ * @method static mixed getByKey(string $key)
+ * @method static void setByKey(string $key , $value , int $expire = null)
+ * @method static void deleteByKey(string $key)
+ */
 class Cache extends Facade
 {
     protected static function getMainClass(): string
